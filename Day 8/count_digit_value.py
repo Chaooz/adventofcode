08bf624872df0ea16b294b47b1479287abc6a7f8
Simon Bruce-Cassidy <55184086+Snoldinator@users.noreply.github.com>
2021-12-13 23:46:20 +0100

    Completed Day 8

--- Day 8/count_digit_value.py
+++ Day 8/count_digit_value.py
@@ -16,41 +16,120 @@ for line in string_list:
     tuple_list.append(new_tuple)
 
 # Save the portion after | to a new list
-output_list = list()
+output_display = list()
+output_message = list()
 for tuple in tuple_list:
-    output_list.append(tuple[1])
+    output_message.append(tuple[0])
+    output_display.append(tuple[1])
 
 # Split the output list into a list of individual words
-output_word_list = list()
-for string in output_list:
+output_display_number_list = list()
+for string in output_display:
     string = string.strip("\n")
-    string = string.split(",")
-    for substring in string:
-        output_word_list.append(substring)
+    string = string.lstrip()
+    string = string.rstrip()
+    output_display_number_list.append(string)
 
-print(output_word_list)
+output_message_list = list()
+for string in output_message:
+    string = string.strip("\n")
+    string = string.lstrip()
+    string = string.rstrip()
+    output_message_list.append(string)
 
-# Count the number of strings in the list that are 2, 3, 4 and 7 chars long
-#digit_total = 0
-#for word in output_word_list:
-#    if word == "ab":
-#        digit_total += 1
-#    elif word == "gcdfa":
-#        digit_total += 2
-#    elif word == "fbcad":
-#        digit_total += 3
-#    elif word == "eafb":
-#        digit_total += 4
-#    elif word == "cdfbe":
-#        digit_total += 5
-#    elif word == "cdfgeb":
-#        digit_total += 6
-#    elif word == "dab":
-#        digit_total += 7
-#    elif word == "acedgfb":
-#        digit_total += 8
-#    else:
-#        digit_total += 9
-#    print(digit_total)
-    
-#print("Sum of digits: " + str(digit_total))
+#
+# Go through all characters in small_number, f.ex 1 ..which is AB
+#
+def small_number_present_in_big_number(big_number, small_number):
+    for char in small_number:
+        if not char in big_number:
+            return False
+    return True
+
+
+# Map words to numbers
+iterations = 0
+total_sum = 0
+
+for line in output_message_list:
+    number = str()
+    one = str()
+    two = str()
+    three = str()
+    four = str()
+    five = str()
+    six = str()
+    seven = str()
+    eight = str()
+    nine = str()
+    zero = str()
+    unknown_word = list()
+
+    # Hente ut unike
+    word_line = line.split(" ")
+    for word in word_line:
+        if len(word) == 2:
+            one = sorted(word)
+        elif len(word) == 3:
+            seven = sorted(word)
+        elif len(word) == 4:
+            four = sorted(word)
+        elif len(word) == 7:
+            eight = sorted(word)
+
+    # De uten avhengigheter    
+    for word in word_line:
+        if small_number_present_in_big_number(word, one):
+            if len(word) == 5:
+                three = sorted(word)
+            elif len(word) == 6:
+                if small_number_present_in_big_number(word,four):
+                    nine = sorted(word)
+                else:
+                    zero = sorted(word)
+        elif len(word) == 6:
+            six = sorted(word)
+        else:
+            unknown_word.append(word)
+
+    # De med avhengigheter
+    for word in unknown_word:
+        if small_number_present_in_big_number(six, word):
+            five = sorted(word)
+        else:
+            two = sorted(word)
+    ## print (one + "-" + two + "-" +  three + "-" + four + "-" + five + "-" + six + "-" + seven + "-" + eight + "-" + nine + "-" + zero)
+
+    digit_string = str()
+    word_line = output_display_number_list[iterations].split(" ")
+    for word in word_line:
+        word = sorted(word)
+        if word == one:
+            digit_string += "1"
+        elif word == two:
+            digit_string += "2"
+        elif word == three:
+            digit_string += "3"
+        elif word == four:
+            digit_string += "4"
+        elif word == five:
+            digit_string += "5"
+        elif word == six:
+            digit_string += "6"
+        elif word == seven:
+            digit_string += "7"
+        elif word == eight:
+            digit_string += "8"
+        elif word == nine:
+            digit_string += "9"
+        elif word == zero:
+            digit_string += "0"
+        else:
+            print("Unknown number found - Something has epically failed!")
+            break
+    #print(digit_string)
+    total_sum += (int(digit_string))
+    iterations += 1
+
+print("The total of all the numbers on the 4 digit panel is: " + str(total_sum))
+print("Thank you, and good night.")
