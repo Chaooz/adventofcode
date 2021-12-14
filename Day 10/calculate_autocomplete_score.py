4332d23ede43a18829bbdbe569a8130496c588d6
Simon Bruce-Cassidy <55184086+Snoldinator@users.noreply.github.com>
2021-12-15 00:32:53 +0100

    OMFG, why will this shite not work?

---
+++ Day 10/calculate_autocomplete_score.py
@@ -0,0 +1,132 @@
+counter = 0
+incomplete_lines = list()
+total_score = 0
+scores = list()
+scoringString = str()
+EOL = False
+
+def loadfile(filename):
+    file = open(filename)
+    lines = file.readlines()
+    file.close()
+    return lines
+
+def getCharScore(char):
+    if char == "(":
+        score = 1
+    elif char == "[":
+        score = 2
+    elif char == "{":
+        score = 3
+    elif char == "<":
+        score = 4
+    else:
+        score = 0
+
+    return score
+
+def getLineScore(charList):
+    lineScore = 0
+
+    for char in charList:
+        lineScore = lineScore * 5
+        lineScore += getCharScore(char)
+
+    return lineScore
+
+def checkCharMatch(startChar, endChar):
+    if startChar == "(" and endChar == ")":
+        return True
+    elif startChar == "[" and endChar == "]":
+        return True
+    elif startChar == "<" and endChar == ">":
+        return True
+    elif startChar == "{" and endChar == "}":
+        return True
+    else:
+        return False
+
+def checkChar(line):
+    global counter
+    global total_score
+    global incomplete_lines
+
+    while counter < len(line):
+    
+        startChar = line[counter]
+        counter += 1
+        if startChar == "(" or startChar == "{" or startChar == "[" or startChar == "<":
+            endChar = checkChar(line)
+            if ( endChar == "-"):
+                return "-"
+            if ( endChar == "\n" ):
+                return "-"
+            elif not checkCharMatch(startChar, endChar):
+                return "-"
+        else:
+            return startChar
+    
+    incomplete_lines.append(line)
+    return "-"
+
+def checkLines(filename):
+    global counter
+    lines = loadfile(filename)
+
+    for line in lines:
+        line = line.rstrip()
+        line = line.strip("\n")
+        counter = 0
+        checkChar(line)
+
+def autocompleteChar(line):
+    global counter
+    global total_score
+    global EOL
+    global scoringString
+
+    while counter < len(line):
+    
+        startChar = line[counter]
+        counter += 1
+        if EOL:
+            return startChar
+
+        if startChar == "(" or startChar == "{" or startChar == "[" or startChar == "<":
+            endChar = autocompleteChar(line)
+            if endChar == "-":
+                scoringString = startChar + scoringString
+                return "-"
+        else:
+            return startChar
+    
+    EOL = True
+    return "-"
+
+def completeLines(lines):
+    global counter
+    global total_score
+    global EOL
+    global scoringString
+
+    for line in lines:
+        counter = 0
+        total_score = 0
+        EOL = False
+        scoringString = str()
+        autocompleteChar(line)
+        #print(scoringString)
+        #print(len(scoringString))
+        #print(line)
+        scores.append(getLineScore(scoringString))
+
+checkLines("nav_subsystem.txt")
+#print(incomplete_lines)
+completeLines(incomplete_lines)
+
+#print(scores)
+scores.sort()
+#print(scores)
+final_score = scores[int(len(scores) / 2)]
+
+print(final_score)
