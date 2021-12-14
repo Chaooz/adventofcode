45ab72207caca80b8ef9337d7481f9677baca36a
Simon Bruce-Cassidy <55184086+Snoldinator@users.noreply.github.com>
2021-12-14 18:38:22 +0100

    Completed day9 part 1

--- Day 9/calculate_lowpoint_risk.py
+++ Day 9/calculate_lowpoint_risk.py
@@ -17,7 +17,45 @@ def createMatrix(textfile):
     return matrix
 
 def findMatrixBoundries(matrix):
-    
+    x = len(matrix)
+    y = len(matrix[0])
+    ret = (x,y)
+    return ret
 
-def findLowpoints(matrix):
-    for 
+def findRisk(matrix):
+    boundries = findMatrixBoundries(matrix)
+    lowpoints = 0
+
+    for x in range(boundries[0]):
+        for y in range(boundries[1]):
+            cell = matrix[x][y]
+            
+            if y-1 < 0:
+                over_cell = 11
+            else:
+                over_cell = matrix[x][y-1]
+
+            if y+2 > boundries[1]:
+                under_cell = 11
+            else:
+                under_cell = matrix[x][y+1]
+
+            if x-1 < 0:
+                left_cell = 11
+            else:
+                left_cell = matrix[x-1][y]
+
+            if x+2 > boundries[0]:
+                right_cell = 11
+            else:
+                right_cell = matrix[x+1][y]
+
+            if cell < over_cell and cell < under_cell and cell < left_cell and cell < right_cell:
+                lowpoints += cell + 1
+
+    return lowpoints
+
+heightmap = createMatrix("height_map.txt")
+risk_score = findRisk(heightmap)
+
+print("The risk score is " + str(risk_score))
