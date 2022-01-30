3aea6844c19ae6c126e1a8ba8c9043c544469988
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2022-01-30 20:46:12 +0100

    Day 18 : Work in progress

--- Day 18/thor_day18_puzzle.py
+++ Day 18/thor_day18_puzzle.py
@@ -14,49 +14,17 @@ sys.path.insert(1, '../Libs')
 from advent_libs import *
 from advent_libs_matrix import *
 
-# Depricated
-def parse_line(line, i, level):
-    number_list = list()
-    startPos = -1
-    txt = ""
-
-    spacer = ""
-    for n in range(0,level):
-        spacer += "-"
-
-    print(spacer + " parse-level:" + str(level) + " i:" + str(i))
-    while i < len(line):
-        startChar = line[i]
-        i += 1
-        if startChar == "[":
-            i, sub_list = parse_line(line,i,level + 1)
-            number_list.append(sub_list)
-            print("char:[  = " + str(sub_list))
-        elif startChar == "]":
-            print("char:]  = " + txt)
-            number_list.append(txt)
-            break
-        elif startChar == ",":
-            print("char:,  = " + txt)
-            if len(txt) > 0:
-                number_list.append(txt)
-            txt = ""
-        else:
-            txt += startChar
-
-    return i, number_list
-
-# If a level 4 occurs ( 4 [[[[ ) ..fex [[[[0,1 then
-# left number (0) is added to parent left number
-# right number (1) is added to parent right number
-# exploded number becomes 0
-
 global left_number
 global right_number
 global trigger_explode
 global debug
 
-def explode_tree( tree_list, level ):
+# Explode Tree
+# If a level 4 occurs ( 4 [[[[ ) ..fex [[[[0,1 then
+# left number (0) is added to parent left number
+# right number (1) is added to parent right number
+# exploded number becomes 0
+def explode_tree( tree_list, level, show_debug ):
     global left_number
     global right_number
     global trigger_explode
@@ -73,7 +41,7 @@ def explode_tree( tree_list, level ):
     for number in tree_list:
         replace_index = 1-i
         if isinstance(number,list):
-            number, left, right, remove_child = explode_tree(number,level + 1)
+            number, left, right, remove_child = explode_tree(number,level + 1, show_debug)
 
             #if left_number > - 1:
             #    print("subexplode left3:" + str(left_number) + ":" + str(tree_list[replace_index]))
@@ -85,12 +53,14 @@ def explode_tree( tree_list, level ):
             key_left = tree_list[0]
             key_right = tree_list[1]
             if left > -1 and isinstance(key_left,int):
-                print("subexplode left-up:" + str(key_left) + " + " + str(left_number) + " = " + str(key_left+left_number))
+                if show_debug:
+                    print("subexplode left-up:" + str(key_left) + " + " + str(left_number) + " = " + str(key_left+left_number))
                 tree_list[0] += left_number
                 left_number = -1
                 left = -1
             elif right > -1 and isinstance(key_right,int):
-                print("subexplode right-up:" + str(key_right) + " + " + str(right_number) + " = " + str(key_right+right_number))
+                if show_debug:
+                    print("subexplode right-up:" + str(key_right) + " + " + str(right_number) + " = " + str(key_right+right_number))
                 tree_list[1] += right_number
                 right_number = -1
                 right = -1
@@ -104,23 +74,32 @@ def explode_tree( tree_list, level ):
                 left_number = tree_list[0]
                 right_number = tree_list[1]
                 trigger_explode = False
-                print("Explode keypair: [" + str(left_number) + "," + str(right_number) + "]")
+                if show_debug:
+                    print("Explode keypair: [" + str(left_number) + "," + str(right_number) + "]")
                 debug = tree_list
                 return tree_list, left_number, right_number, True
         elif b == False:
             if right_number > -1 :
                 tree_list[i] = number + right_number
-                print("subexplode right-down:" + str(number) + " + " + str(right_number) + " = " + str(number+right_number))
+                if show_debug:
+                    print("subexplode right-down:" + str(number) + " + " + str(right_number) + " = " + str(number+right_number))
                 right_number = -1
-            elif left_number > -1 :
+            elif left_number == -12 :
                 tree_list[i] = number + left_number
-                print("subexplode left-down:" + str(number) + " + " + str(left_number) + " = " + str(number+left_number))
+                if show_debug:
+                    print("subexplode left-down:" + str(number) + " + " + str(left_number) + " = " + str(number+left_number))
                 left_number = -1
 
         i += 1
 
     return tree_list, left, right, False
 
+# Split Tree
+# If a number is 10 or higher, the number is split into a key pair
+# divided by 2.
+# left number is rounded down
+# right number is rounded up
+# 11 => [ 5, 6 ]
 def split_tree( tree_list, did_split ):
     i = 0
     for number in tree_list:
@@ -138,7 +117,35 @@ def split_tree( tree_list, did_split ):
 
     return tree_list, did_split
 
-def run_explode_input( input ):
+# Add Numbers
+# Add two numbers
+# 
+def add_numbers( number1, number2, show_debug ):
+    result = "[" + number1 + "," + number2 + "]"
+    if show_debug:
+        print("-------------------------------------------------------------------")
+        print("  " + str(number1))
+        print("+ " + str(number2))
+        print("= " + str(result))
+        print("-------------------------------------------------------------------")
+    return result
+
+# Sum Tree List
+# Add the numbers together.
+# First number is multiplied by 3
+# Second number is multiplied by 2
+def sum_tree_list( number_list ):
+    total = 0
+    i = 3
+    for number in number_list:
+        if isinstance(number,list):
+            number = sum_tree_list(number)
+        total += number * i
+        i -= 1
+    return total
+
+
+def run_explode_input( input, show_debug ):
 
     global left_number
     global right_number
@@ -150,36 +157,27 @@ def run_explode_input( input ):
 
     tree = ast.literal_eval(input)
 #    print_tree_list("",tree)
-    new_tree, remove, l,r = explode_tree(tree,0)
+    new_tree, remove, l,r = explode_tree(tree,0, show_debug)
     tree_str = str(new_tree)
     tree_str = tree_str.replace(" ", "")
 
-    if left_number > -1:
-        print("could ot replace left?" + str(left_number))
+    if left_number > -1 and show_debug:
+        print("could not replace left?" + str(left_number))
 
     return tree_str
 
-def run_explode_input_full( input ):
+def run_explode_input_full( input,show_debug ):
     while(True):
-        result = run_explode_input(input)
+        result = run_explode_input(input,show_debug)
         if result == input:
             return result
         print("After explode:" + str(result))
         input = result
     return input
 
-def add_numbers( number1, number2 ):
-    result = "[" + number1 + "," + number2 + "]"
-    print("-------------------------------------------------------------------")
-    print("  " + str(number1))
-    print("+ " + str(number2))
-    print("= " + str(result))
-    print("-------------------------------------------------------------------")
-    return result
-
 def run_add_numbers(input, parameters):
-    number1,number2 = parameters
-    return add_numbers(number1,number2)
+    show_debug, number1,number2 = parameters
+    return add_numbers(number1,number2, show_debug)
 
 def run_split_input(input):
     tree = ast.literal_eval(input)
@@ -190,15 +188,6 @@ def run_split_input(input):
     tree_str = tree_str.replace(" ", "")
     return tree_str
 
-def sum_tree_list( number_list ):
-    total = 0
-    i = 3
-    for number in number_list:
-        if isinstance(number,list):
-            number = sum_tree_list(number)
-        total += number * i
-        i -= 1
-    return total
 
 def run_sum_input(input):
     tree = ast.literal_eval(input)
@@ -210,7 +199,7 @@ def run_sum_input(input):
 #    for line in data:
 #        do_calc(line)
 
-def run_full_calc(input):
+def run_full_calc(input, show_debug):
 
     #input = run_add_numbers(input, parameters)
 
@@ -219,13 +208,14 @@ def run_full_calc(input):
 
         exit_loop = False
         while(not exit_loop):
-            result = run_explode_input(input)
+            result = run_explode_input(input,show_debug)
             txt = input
             if not debug is None:
                 highlight = str(debug)
                 highlight = highlight.replace(" ", "")
                 txt = string_highlight(input,highlight, bcolors.WARNING)
-            print("run_explode_input:" + str(txt) + " => " + str(result))
+            if show_debug:
+                print("run_explode_input:" + str(txt) + " => " + str(result))
 
             if input == result:
                 exit_loop = True
@@ -234,7 +224,8 @@ def run_full_calc(input):
         exit_loop = False
         while(not exit_loop):
             result = run_split_input(input)
-            print("run_split_input:" + str(input) + " => " + str(result))
+            if show_debug:
+                print("run_split_input:" + str(input) + " => " + str(result))
             if input == result:
                 exit_loop = True
             input = result
@@ -244,22 +235,24 @@ def run_full_calc(input):
 
     return input
 
-def run_sum_from_file(filename):
+def run_sum_from_file(filename, show_debug):
     lines = loadfile(filename)
     line = lines[0].replace("\n","")
     for i in range(1,len(lines)):
         line2 = lines[i].replace("\n","")
-        line = add_numbers( line, line2 )
-        result = run_full_calc(line) 
+        line = add_numbers( line, line2, show_debug )
+        result = run_full_calc(line,show_debug) 
         v = str(result)
-        print(v)
+        if show_debug:
+            print(v)
         break
        
     return result
 
-# Basic calc unittests
-unittest(run_sum_input,29,"[9,1]")
-unittest(run_sum_input,21,"[1,9]")
+# Function tests
+show_debug = False
+unittest(run_sum_input, 29,"[9,1]")
+unittest(run_sum_input, 21,"[1,9]")
 unittest(run_sum_input, 143, "[[1,2],[[3,4],5]]")
 unittest(run_sum_input, 1384, "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
 unittest(run_sum_input, 445, "[[[[1,1],[2,2]],[3,3]],[4,4]]")
@@ -267,21 +260,22 @@ unittest(run_sum_input, 791, "[[[[3,0],[5,3]],[4,4]],[5,5]]")
 unittest(run_sum_input, 1137, "[[[[5,0],[7,4]],[5,5]],[6,6]]")
 unittest(run_sum_input, 3488, "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")
 
-unittest(run_explode_input, "[[[[0,9],2],3],4]", "[[[[[9,8],1],2],3],4]")
-unittest(run_explode_input, "[7,[6,[5,[7,0]]]]", "[7,[6,[5,[4,[3,2]]]]]")
-unittest(run_explode_input, "[[6,[5,[7,0]]],3]", "[[6,[5,[4,[3,2]]]],1]")
-unittest(run_explode_input, "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")
-unittest(run_explode_input, "[[3,[2,[8,0]]],[9,[5,[7,0]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
-unittest(run_explode_input, "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]")
-unittest(run_explode_input, "[[[[0,7],4],[15,[0,13]]],[1,1]]", "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]")
+unittest_input(run_explode_input, show_debug, "[[[[0,9],2],3],4]", "[[[[[9,8],1],2],3],4]")
+unittest_input(run_explode_input, show_debug,  "[7,[6,[5,[7,0]]]]", "[7,[6,[5,[4,[3,2]]]]]")
+unittest_input(run_explode_input, show_debug, "[[6,[5,[7,0]]],3]", "[[6,[5,[4,[3,2]]]],1]")
+unittest_input(run_explode_input, show_debug, "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")
+unittest_input(run_explode_input, show_debug, "[[3,[2,[8,0]]],[9,[5,[7,0]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+unittest_input(run_explode_input, show_debug, "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]")
+unittest_input(run_explode_input, show_debug, "[[[[0,7],4],[15,[0,13]]],[1,1]]", "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]")
 
 unittest(run_split_input, "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", "[[[[0,7],4],[15,[0,13]]],[1,1]]")
 unittest(run_split_input, "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]")
 
-#unittest_input(run_add_numbers, ("[[[[4,3],4],4],[7,[[8,4],9]]]", "[1,1]"), "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "")
-#unittest(run_full_calc, "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]")
+unittest_input(run_add_numbers, (show_debug,"[[[[4,3],4],4],[7,[[8,4],9]]]", "[1,1]"), "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "")
 
+unittest_input(run_full_calc, show_debug, "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]")
 
-unittest(run_sum_from_file, "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", "unittest2_day18_data.txt")
+unittest_input(run_sum_from_file, show_debug, "[[[[5,0],[7,4]],[5,5]],[6,6]]", "unittest1_day18_data.txt")
+
+unittest_input(run_sum_from_file, True, "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", "unittest2_day18_data.txt")
 #unittest(run_sum_from_file, "[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]", "unit1.txt")
-
