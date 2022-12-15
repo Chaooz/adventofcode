5f2e58c79c99e0cf7951a60b2919a8828ad7ec36
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2022-12-15 18:36:08 +0100

    Day12 puzzle 2

--- 2022/Day 12/Thor/solution.py
+++ 2022/Day 12/Thor/solution.py
@@ -10,53 +10,53 @@ from advent_libs_list import *
 from advent_libs_vector2 import *
 from advent_libs_pathfinding import *
 
-# Rules
-# Start pos : S
-# End pos :E
-# Can only go 1 up or equal
-
-print("")
-print_color("Day 12: Hill Climbing Algorithm", bcolors.OKGREEN)
-print("")
-
-#
-# Finds a character in the matrix and returns position
-#
-def getPosition(matrix:Matrix,character:str) -> Vector2:
-    for y in range(0,matrix.sizeY):
-        for x in range(0,matrix.sizeX):
-            if matrix.Get(x,y) == character:
-                return Vector2(x,y)
-    return Vector2(-1,-1)
-
 def solvePuzzle1(filename):
+    return solvePuzzle(filename, 1)
+
+def solvePuzzle2(filename):
+    return solvePuzzle(filename, 2)
+
+def solvePuzzle(filename, puzzleNumber):
     matrix  = Matrix.CreateFromFile(filename, ".")
 
-    startPos = getPosition(matrix,"S")
-    endPos = getPosition(matrix,"E")
-
-    valueList = list()
-    valueList.append(26)
-
+    #
+    # Task:
     # Remap the heighmap into numbers
+    # Find startpoints and endpoint
+    #
+    startPostList = Vector2List()
     for y in range(0,matrix.sizeY):
         for x in range(0,matrix.sizeX):
             character = matrix.Get(x,y)
             if character == "S":
                 matrix.Set(x,y, 0)
-                pass
+                startPostList.append( Vector2(x,y) )
+            elif character == "a" and puzzleNumber == 2:
+                startPostList.append( Vector2(x,y) )
+                matrix.Set(x,y, 0)
             elif character == "E":
+                endPos = Vector2(x,y)
                 matrix.Set(x,y, 26) # z value
-                pass
             else:
                 number = int(ord(character) - ord("a"))
                 matrix.Set(x,y,number)
 
-    pathfinding = Pathfinding()
-    shortestPath = pathfinding.HeuristicAstarPathTo( matrix, startPos, endPos, pathfinding.oneStepPathRule )
-    #pathfinding.DebugPrintPath(matrix,shortestPath)
-    return len(shortestPath) - 1
+    shortestPath = 999
+    for startPos in startPostList:
+        #print("Path between " + startPos.ToString() + " => " + endPos.ToString())
+        pathfinding = Pathfinding()
+        pathList = pathfinding.HeuristicAstarPathTo( matrix, startPos, endPos, pathfinding.oneStepPathRule )
+        #pathfinding.DebugPrintPath(matrix,shortestPath)
+        if len(pathList) > 0 and len(pathList) < shortestPath:
+            shortestPath = len(pathList)
+    return shortestPath - 1
 
-#unittest(solvePuzzle1, 31, "unittest.txt")
+print("")
+print_color("Day 12: Hill Climbing Algorithm", bcolors.OKGREEN)
+print("")
+
+unittest(solvePuzzle1, 31, "unittest.txt")
+unittest(solvePuzzle2, 29, "unittest.txt")
 unittest(solvePuzzle1, 423, "puzzleinput.txt")
-unittest(solvePuzzle1, 31, "puzzleinput_work.txt")
+unittest(solvePuzzle2, 416, "puzzleinput.txt")
+#unittest(solvePuzzle1, 31, "puzzleinput_work.txt")
