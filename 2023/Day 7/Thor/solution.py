25d3c90a67e4626dac0571d34574fcc80de76227
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2023-12-09 16:12:11 +0100

    Refactor done day 7

--- 2023/Day 7/Thor/solution.py
+++ 2023/Day 7/Thor/solution.py
@@ -16,154 +16,80 @@ print("")
 
 CARD_LIST = "123456789ABCDE"
 CARD_MAP = { "T":"A", "J":".", "Q":"C", "K":"D", "A":"E" }
-JOKER_CARD = "1"
             
-class RESULT(Enum):
+class RESULT:
     NONE = 0
     ONE_PAIR = 1
-    ONE_PAIR_JOKER = 2
     TWO_PAIRS = 3
-    TWO_PAIRS_JOKER = 4
     THREE_OF_A_KIND = 5
-    THREE_OF_A_KIND_JOKER = 6
     FULL_HOUSE = 7
-    FULL_HOUSE_JOKER = 8
     FOUR_OF_A_KIND = 9
-    FOUR_OF_A_KIND_JOKER = 10
     FIVE_OF_A_KIND = 11
-    FIVE_OF_A_KIND_JOKER = 12
 
-
-def upgradeHand(current:RESULT, new:RESULT, newJoker):
-#    print("check upgrade:", current, " vs ", new)
-
+def upgradeHand(current:RESULT, new:RESULT):
     if current == RESULT.NONE:
-        ret = new
-        if newJoker:
-            ret = RESULT(new.value + 1)
-        return ret
-
-    oldJoker = current.value % 2 == 0
-
-    # If both are jokers, use highest
-    if ( newJoker and oldJoker ):
-        if current.value > new.value:
-            return current
-        else:
-            return new
-
-    # Mask off jokers
-    if current.value % 2 == 0:
-        current = RESULT( current.value - 1)
-
-    if ( current == RESULT.ONE_PAIR ):
+        return new
+    elif ( current == RESULT.ONE_PAIR ):
         if new == RESULT.ONE_PAIR:
-#            print("upgradeHand:", current, " vs ", new, " =>", RESULT.TWO_PAIRS)
             return RESULT.TWO_PAIRS
         elif new == RESULT.THREE_OF_A_KIND:
-#            print("upgradeHand:", current, " vs ", new, " =>", RESULT.FULL_HOUSE)
             return RESULT.FULL_HOUSE
     elif ( current == RESULT.THREE_OF_A_KIND):
         if new == RESULT.ONE_PAIR:
-#            print("upgradeHand:", current, " vs ", new, " =>", RESULT.FULL_HOUSE)
             return RESULT.FULL_HOUSE
     elif current != RESULT.NONE and new == RESULT.NONE:
-#        print("upgradeHand:", current, " vs ", new, " =>", current)
         return current    
     elif current.value > new.value:
-#        print("upgradeHand:", current, " vs ", new, " =>", current)
         return current
 
- #   print("Use new : current", current, " new:", new)
-        
     return new
 
 def getCardRule(cards:str):
-    return getCardRuleWithJoker(cards, "X")
-
-def getCardRuleWithJoker(cards:str, joker:str):
     result = RESULT.NONE
-
-    numJokers = cards.count(joker)
-    hasJoker = numJokers > 0
-
-#    print("cards:", cards, " jukers:", numJokers, " joker:", joker)
 
     for c in CARD_LIST:
         n = cards.count(c)
-
-        if ( c == joker ):
-            continue
-
-        n += numJokers
-
         if n == 2:
-            result = upgradeHand(result,RESULT.ONE_PAIR, hasJoker)
+            result = upgradeHand(result,RESULT.ONE_PAIR)
         elif n == 3:
-            result = upgradeHand(result,RESULT.THREE_OF_A_KIND, hasJoker)
+            result = upgradeHand(result,RESULT.THREE_OF_A_KIND)
         elif n == 4:
-            result = upgradeHand(result,RESULT.FOUR_OF_A_KIND, hasJoker)            
+            result = upgradeHand(result,RESULT.FOUR_OF_A_KIND)            
         elif n == 5:
-            result = upgradeHand(result,RESULT.FIVE_OF_A_KIND, hasJoker)            
-
-    # Strip joker
-    if result.value > 0 and result.value % 2 == 0:
-        result = RESULT(result.value - 1)
+            result = upgradeHand(result,RESULT.FIVE_OF_A_KIND)            
 
     return result
 
-def insertRanked(sortedCardList:list, cards:str, cardRule:RESULT, strength:int, joker:str):
+def insertRanked(sortedCardList:list, cards:str, card_rank:RESULT, strength:int):
     if len(sortedCardList) == 0:
-        sortedCardList.append((cards,cardRule,strength))
+        sortedCardList.append((cards,card_rank,strength))
         return sortedCardList
 
-    cards_check = cards.replace(joker, "0")
-
     for i in range(0,len(sortedCardList)):
-        (c,r,s) = sortedCardList[i]
+        (c,rank,s) = sortedCardList[i]
 
-        if cardRule.value > r.value:
-            sortedCardList.insert(i,(cards,cardRule,strength))
+        if card_rank > rank:
+            sortedCardList.insert(i,(cards,card_rank,strength))
             return sortedCardList
-        elif cardRule.value == r.value:
-            d = c.replace(joker, "0")
-            if cards_check > c:
-                sortedCardList.insert(i,(cards,cardRule,strength))
+        elif card_rank == rank:
+            if cards > c:
+                sortedCardList.insert(i,(cards,card_rank,strength))
                 return sortedCardList
 
-    sortedCardList.append((cards,cardRule,strength))
+    sortedCardList.append((cards,card_rank,strength))
     return sortedCardList
 
-def replaceValue(cards:str, joker:str ):
+def replaceValue(cards:str ):
     cards = cards.replace("A", "E")
     cards = cards.replace("K", "D")
     cards = cards.replace("Q", "C")
-    cards = cards.replace("J", joker)
+    cards = cards.replace("J", "B")
     cards = cards.replace("T", "A")
     return cards
 
-def solvePuzzle1(filename:str):
-    lines = loadfile(filename)
-
-    sortedCardList = list()
-
-    for line in lines:
-        line = line.replace("\n", "")
-        (cards,strength) = line.split(" ")
-
-        cards = replaceValue(cards, "B")
-        cardRule = getCardRule(cards)
-        sortedCardList = insertRanked(sortedCardList, cards, cardRule, int(strength), "X" )
-
-    sum = 0
-    power = len(sortedCardList)
-    for card in sortedCardList:
-        (cards,cardRule,strength) = card
-        sum += power * strength
-        power -= 1
-
-    return sum
-
+#
+# Return the score of a hand
+#
 def score(hand):
     counts = [hand.count(card) for card in hand]
     if 5 in counts:
@@ -174,43 +100,47 @@ def score(hand):
         if 2 in counts:
             return RESULT.FULL_HOUSE
         return RESULT.THREE_OF_A_KIND
+    elif counts.count(2) == 4:
+        return RESULT.TWO_PAIRS
     elif 2 in counts:
         return RESULT.ONE_PAIR
     return RESULT.NONE
 
-#
-# Part TWO
-# 
-def score(hand):
-    counts = [hand.count(card) for card in hand]
-    # FIVE_OF_A_KIND
-    if 5 in counts:
-        return 6            
-    # FOUR_OF_A_KIND
-    elif 4 in counts:       
-        return 5
-    # THREE_OF_A_KIND
-    elif 3 in counts:
-        # ONE_PAIR
-        if 2 in counts:
-            # FULL_HOUSE
-            return 4
-        return 3
-    # TWO_PAIRS
-    elif counts.count(2) == 4:
-        return 2
-    # ONE_PAIR
-    elif 2 in counts:
-        return 1
-    return 0
+def solvePuzzle1(filename:str):
+    lines = loadfile(filename)
 
-# Classify
+    sortedCardList = list()
+
+    for line in lines:
+        line = line.replace("\n", "")
+        (cards,strength) = line.split(" ")
+
+        cards = replaceValue(cards)
+        cardRule = getCardRule(cards)
+        sortedCardList = insertRanked(sortedCardList, cards, cardRule, int(strength) )
+
+    sum = 0
+    power = len(sortedCardList)
+    for card in sortedCardList:
+        (cards,cardRule,strength) = card
+        sum += power * strength
+        power -= 1
+
+    return sum
+
+#
+# Find the best hand out of all possible hands when replacing the Joker with a different card
+#
 def bestHand(hand):
     return max(map(score,replacements(hand)))
 
+# 
 def strength(hand):
     return (bestHand(hand), [CARD_MAP.get(card,card) for card in hand])
 
+# 
+# Return all possible hands when replacing the Joker with a different card
+#
 def replacements(hand):
     if hand == "":
         return [""]
@@ -230,10 +160,10 @@ def solvePuzzle2(filename:str):
         s = bestHand(hand)
         cardList.append( (hand,s, int(bid)))
 
+    # Sort cardlist with the strength of the hand (play[0] is the hand)
     cardList.sort( key=lambda play: strength(play[0]))
 
-#    print(cardList)
-
+    # Calculate the total score
     total = 0
     for rank, (hand,s,bid) in enumerate(cardList, 1):
         total += rank * bid
@@ -249,8 +179,6 @@ unittest(getCardRule, RESULT.FIVE_OF_A_KIND, "22222")
 
 unittest(solvePuzzle1, 6440, "unittest1.txt")
 unittest(solvePuzzle1, 250951660, "input.txt")     
-
-unittest_input(getCardRuleWithJoker, JOKER_CARD, RESULT.THREE_OF_A_KIND, "22"+JOKER_CARD+"45")
 
 unittest(solvePuzzle2, 5905, "unittest1.txt")
 unittest(solvePuzzle2, 251481660, "input.txt")
