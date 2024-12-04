413a893484b91ef66e85241f332a252d6ce4c562
Simon Bruce-Cassidy <simon@gale-huset.net>
2024-12-04 08:32:56 +0100

    Start of day 4

---
+++ 2024/04/Simon/solution.py
@@ -0,0 +1,10 @@
+#!/usr/lib/python3
+"""
+    Description: File to solve the Advent of Code 2024 Day 4 puzzle
+
+    Returns:
+        str: The solution to the puzzle
+"""
+import sys, re
+sys.path.insert(1, '../../../Libs')
+from advent_libs import loadfile
