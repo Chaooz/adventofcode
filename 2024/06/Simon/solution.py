d38ed9a454d62ab96cf7f4f9617e3f4065480885
Simon Bruce-Cassidy <simon@gale-huset.net>
2024-12-08 00:45:48 +0100

    Simon day 6 complete

--- 2024/06/Simon/solution.py
+++ 2024/06/Simon/solution.py
@@ -5,7 +5,8 @@
     Returns:
         str: The solution to the puzzle
 """
-import sys, re
+import sys
+import re
 sys.path.insert(1, '../../../Libs')
 from advent_libs import *
 from advent_libs_matrix import *
@@ -20,44 +21,44 @@ vector_list = [
     Vector2(-1,0)
 ]
 
-def movement_in_direction(new_direction:str) -> Vector2:
-    match new_direction:
-        case "N":
-            return Vector2(0,-1)
-        case "S":
-            return Vector2(0,1)
-        case "E":
-            return Vector2(1,0)
-        case "W":
-            return Vector2(-1,0)
+def movement_in_direction(new_direction:int) -> Vector2:
+    if new_direction == 0:
+        return Vector2(0,-1) # North
+    elif new_direction == 1:
+        return Vector2(1,0) # East
+    elif new_direction == 2:
+        return Vector2(0,1)
+    elif new_direction == 3:
+        return Vector2(-1,0)
+
 
 def findPath(matrix:Matrix, current_position:Vector2):
-    directions = ["N", "E", "S", "W"]
+#    directions = ["N", "E", "S", "W"]
     current_direction_index = 0
-    current_direction_value = directions[current_direction_index]
+#    current_direction_value = directions[current_direction_index]
     out_of_bounds = False
     visited_points = []
 
-    while not out_of_bounds:
-        direction_vector = movement_in_direction(current_direction_value)
-        #direction_vector = vector_list[current_direction_index]
+    while True:
+        #direction_vector = movement_in_direction(current_direction_value)
+        direction_vector = movement_in_direction(current_direction_index)
         new_position = current_position + direction_vector
 
         # Check if we are outside of matrix                       
         if matrix.IsOutOfBounds(new_position):
-            out_of_bounds = True
-            break
+            return visited_points
 
         data = matrix.GetPoint(new_position)
         if data == "#":
             # Turn and stuff
             current_direction_index = (current_direction_index + 1) % 4
-            current_direction_value = directions[current_direction_index]
+#            current_direction_value = directions[current_direction_index]
+
         else:
             current_position = new_position
-            position_and_direction = (current_position, current_direction_value)
 
             # Add newPositon into list
+            position_and_direction = (current_position, current_direction_index)
             if position_and_direction not in visited_points:
                 visited_points.append(position_and_direction)
             else:
@@ -66,34 +67,26 @@ def findPath(matrix:Matrix, current_position:Vector2):
 
     return visited_points    
 
-#
-# See if we can path 
-#
 def checkPath(matrix:Matrix, current_position:Vector2):
-    directions = ["N", "E", "S", "W"]
     current_direction_index = 0
-    current_direction_value = directions[current_direction_index]
     out_of_bounds = False
     visited_points = []
 
-    while not out_of_bounds:
-        direction_vector = movement_in_direction(current_direction_value)
-        #direction_vector = vector_list[current_direction_index]
+    while True:
+        direction_vector = movement_in_direction(current_direction_index)
         new_position = current_position + direction_vector
 
         # Check if we are outside of matrix                       
         if matrix.IsOutOfBounds(new_position):
-            out_of_bounds = True
-            break
+            return visited_points
 
         data = matrix.GetPoint(new_position)
         if data == "#":
             # Turn and stuff
             current_direction_index = (current_direction_index + 1) % 4
-            current_direction_value = directions[current_direction_index]
 
             # Add newPositon into list
-            position_and_direction = (current_position, current_direction_value)
+            position_and_direction = (current_position, current_direction_index)
             if position_and_direction not in visited_points:
                 visited_points.append(position_and_direction)
             else:
@@ -102,8 +95,6 @@ def checkPath(matrix:Matrix, current_position:Vector2):
 
         else:
             current_position = new_position
-
-    return visited_points    
 
 def move_through_room(filename:str):
     matrix = Matrix.CreateFromFile(filename,".")
@@ -125,6 +116,7 @@ def create_loopy_guard(filename:str):
     
     unique_obsticles = []
     run_cycles = 0
+    sum = 0
     for index in range(1,len(visited_points)):
         run_cycles = run_cycles + 1
         if run_cycles % 100 == 0:
@@ -132,12 +124,16 @@ def create_loopy_guard(filename:str):
 
         visited_position_value = visited_points[index][0]
 
+        if matrix.GetPoint(visited_position_value) != ".":
+            continue
+
         matrix.SetPoint(visited_position_value, "#")
-        new_path = findPath(matrix, guard_start_position)
+        new_path = checkPath(matrix, guard_start_position)
         matrix.SetPoint(visited_position_value, ".")
 
         # We detected a loop!
         if new_path == None:
+            sum += 1
             if visited_position_value not in unique_obsticles:
                 unique_obsticles.append(visited_position_value)
 
