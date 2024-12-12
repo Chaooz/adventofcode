cd0e857b375e30427652e2747d87687aead89d51
Thor Hanssen <thor.richard.hansen@gmail.com>
2024-12-12 09:36:59 +0100

    2024 Thor Day 12 - Created files

---
+++ 2024/12/Thor/solution.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+# https://adventofcode.com/2024/day/12
+
+import sys
+import re
+
+# Import custom libraries
+sys.path.insert(1, '../../../Libs')
+sys.path.insert(1, '../Libs')
+
+from advent_libs import *
+
+setupCode("Day 0: Template")
+
+def solvePuzzle1(filename):
+    return 0
+
+def solvePuzzle2(filename):
+    return 0
+
+unittest(solvePuzzle1, -1, "unittest1.txt")
+unittest(solvePuzzle1, 1930, "unittest2.txt")
+#unittest(solvePuzzle2, -1, "unittest1.txt")
+
+#runCode(12,solvePuzzle1, -1, "input.txt")
+#runCode(12,solvePuzzle2, -1, "input.txt")
