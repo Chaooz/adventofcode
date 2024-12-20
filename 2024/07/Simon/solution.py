a16d1aa4b80c9b0478a01414995d072c6ab5d7ea
Simon Bruce-Cassidy <simon@gale-huset.net>
2024-12-20 20:35:54 +0100

    Day 7 start Simon

---
+++ 2024/07/Simon/solution.py
@@ -0,0 +1,10 @@
+#!/usr/lib/python3
+"""
+    Description: Script to solve the Advent of Code 2024 Day 7 puzzle
+
+    Returns:
+        str: The solution to the puzzle
+"""
+import sys
+sys.path.insert(1, '../../../Libs')
+from advent_libs_matrix import *
