cabfcafee63d0ec4ce6cf41d8cdea8e0b198be1c
Thor Hanssen <thor.richard.hansen@gmail.com>
2024-12-25 12:02:10 +0100

    Thor Day 20 Part1 - Changed logic to be more dynamic

--- 2024/20/Thor/solution.py
+++ 2024/20/Thor/solution.py
@@ -14,7 +14,13 @@ from advent_libs_pathfinding_astar import *
 
 setupCode("Day 20: Race Condition")
 
-def solveMaze(filename, minCheatSaved):
+class SYMBOLS:
+    OPEN_SLOT = "."
+    NORMAL_PATH = "o"
+    SCANNING_PATH = "O"
+    WALL = "#"
+
+def solveMaze(filename, maxCheatTime, minCheatSaved):
     matrix = Matrix.CreateFromFile(filename)
     pathfinding = Pathfinding(DefaultPathfindingRuleSet())
 
@@ -30,7 +36,7 @@ def solveMaze(filename, minCheatSaved):
 
     # Create a path with position as keys
     for node in nodePathList:
-        matrix.SetPoint(node.position, "o")
+        matrix.SetPoint(node.position, SYMBOLS.NORMAL_PATH)
         posPaths[node.position] = node
 
     for index in range(0, len(nodePathList)):
@@ -38,31 +44,33 @@ def solveMaze(filename, minCheatSaved):
         checkedPath.append(currentNode.position)
 
         # Mark the spot where we are so we can se where we have checked the path
-        matrix.SetPoint(currentNode.position, "O")
+        matrix.SetPoint(currentNode.position, SYMBOLS.SCANNING_PATH)
 
         # Go all 4 corners and check for other path nodes
         for pos, dir in DefaultPathfindingRuleSet.default_directions:
 
-            # The position next to ours must be a wall
-            wallPos = currentNode.position + dir
-            if matrix.IsOutOfBounds(wallPos) or (matrix.GetPoint(wallPos) != "#" and matrix.GetPoint(wallPos) != "|"):
-                continue
+            currentPos = currentNode.position
+            data ="#"
+            for cheatIndex in range(0,maxCheatTime):
 
-            # The position 2 steps away must an unpathed position in the path
-            newPos = currentNode.position + dir * 2
-            if matrix.IsOutOfBounds(newPos) or matrix.GetPoint(newPos) != "o":
-                continue
+                currentPos = currentPos + dir
+                if matrix.IsOutOfBounds(currentPos):
+                    break
 
-            # Mark this positio
-            matrix.SetPoint(wallPos, "|")
+                # IF we go back to a different part of the path
+                data = matrix.GetPoint(currentPos)
+                if data == SYMBOLS.NORMAL_PATH or data == SYMBOLS.SCANNING_PATH:
+                    break
 
-            node = posPaths.get(newPos)
-            newIndex = nodePathList.index(node)
-            newPathLength = index + 1 + (len(nodePathList) - newIndex)
-            saved = len(nodePathList) - newPathLength - 1
+            if data == "o":
+                node = posPaths.get(currentPos)
+                newIndex = nodePathList.index(node)
+                newPathLength = index + 1 + (len(nodePathList) - newIndex)
+                saved = len(nodePathList) - newPathLength - 1
 
-            if saved >= minCheatSaved:
-               sum += 1
+                if saved >= minCheatSaved:
+                    sum += 1
+                    #break
 
     matrix.SetPoint(startPos, "S")
     matrix.SetPoint(endPos, "E")
@@ -72,7 +80,7 @@ def solveMaze(filename, minCheatSaved):
     colorList.append(("#", bcolors.DARK_GREY))
     colorList.append(("o", bcolors.YELLOW))
     colorList.append(("O", bcolors.YELLOW))
-    colorList.append(("|", bcolors.DARK_GREY))
+    colorList.append(("0", bcolors.DARK_GREY))
     colorList.append(("S", bcolors.WHITE))
     colorList.append(("E", bcolors.WHITE))
 #    matrix.PrintWithColor(colorList, bcolors.DARK_GREY, "", "")
@@ -80,13 +88,16 @@ def solveMaze(filename, minCheatSaved):
     return sum
 
 def solveUnittest1(filename):
-    return solveMaze(filename, 1)
+    return solveMaze(filename, 2, 1)
 
 def solvePuzzle1(filename):
-    return solveMaze(filename, 100)
+    return solveMaze(filename, 2, 100)
+
+def solveUnittest2(filename):
+    return solveMaze(filename, 20, 50)
 
 def solvePuzzle2(filename):
-    return 0
+    return solveMaze(filename, 20, 100)
 
 unittest(solveUnittest1, 44, "unittest1.txt")
 #unittest(solvePuzzle2, -1, "unittest1.txt")
