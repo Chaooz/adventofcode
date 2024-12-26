5564447ec0d388367fa1a33574ae51ddbbe83444
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2024-12-26 14:01:16 +0100

    2024 Thor Day 20 - Part 2 + cleanup

--- 2024/20/Thor/solution.py
+++ 2024/20/Thor/solution.py
@@ -20,7 +20,8 @@ class SYMBOLS:
     SCANNING_PATH = "O"
     WALL = "#"
 
-def solveMaze(filename, maxCheatTime, minCheatSaved):
+
+def solveMaze(filename, maxCheatTime, minCheatSaved, debug):
     matrix = Matrix.CreateFromFile(filename)
     pathfinding = Pathfinding(DefaultPathfindingRuleSet())
 
@@ -28,78 +29,73 @@ def solveMaze(filename, maxCheatTime, minCheatSaved):
     endPos = matrix.FindFirst("E")
     
     nodePathList = pathfinding.AStarPathTo(matrix, startPos, endPos)
-
     posPaths = dict()
-    checkedPath = []
-
     sum = 0
 
     # Create a path with position as keys
     for index in range(0,len(nodePathList)):
         node = nodePathList[index]
+        # Place an "o" on the path
         matrix.SetPoint(node.position, SYMBOLS.NORMAL_PATH)
+        # Create a dictory where we can look up the position and get the pathlength to that position
         posPaths[node.position] = index
 
     for index in range(0, len(nodePathList)):
         currentNode = nodePathList[index]
-        checkedPath.append(currentNode.position)
 
         # Mark the spot where we are so we can se where we have checked the path
         matrix.SetPoint(currentNode.position, SYMBOLS.SCANNING_PATH)
 
         # Go all 4 corners and check for other path nodes
-        for pos, dir in DefaultPathfindingRuleSet.default_directions:
+        for posY in range(-maxCheatTime, maxCheatTime + 1):
+            # To make sure that the total length is not longer than maxCheatTime
+            maxX = maxCheatTime - abs(posY)
+            for posX in range(-maxX, maxX+1):
+                currentPos = currentNode.position + Vector2(posX, posY)
 
-            currentPos = currentNode.position
-            data ="#"
-            for cheatIndex in range(0,maxCheatTime):
-
-                currentPos = currentPos + dir
                 if matrix.IsOutOfBounds(currentPos):
-                    break
+                    continue
 
-                # IF we go back to a different part of the path
-                data = matrix.GetPoint(currentPos)
-                if data == SYMBOLS.NORMAL_PATH or data == SYMBOLS.SCANNING_PATH:
-                    break
+                # If we go back to a different part of the path
+                if matrix.GetPoint(currentPos) == SYMBOLS.NORMAL_PATH:
 
-            if data == "o":
-                pathLen = posPaths.get(currentPos)
-                newPathLength = index + 1 + (len(nodePathList) - pathLen)
-                saved = len(nodePathList) - newPathLength - 1
+                    pathStart = posPaths.get(currentNode.position)
+                    pathEnd = posPaths.get(currentPos)
+                    picoLen = abs(posX) + abs(posY)
+                    saved = pathEnd - pathStart - picoLen
 
-                if saved >= minCheatSaved:
-                    sum += 1
-                    #break
-
-    matrix.SetPoint(startPos, "S")
-    matrix.SetPoint(endPos, "E")
+                    if saved >= minCheatSaved:
+                        sum += 1
 
     # Overengineering 101
-    colorList = list()
-    colorList.append(("#", bcolors.DARK_GREY))
-    colorList.append(("o", bcolors.YELLOW))
-    colorList.append(("O", bcolors.YELLOW))
-    colorList.append(("0", bcolors.DARK_GREY))
-    colorList.append(("S", bcolors.WHITE))
-    colorList.append(("E", bcolors.WHITE))
-#    matrix.PrintWithColor(colorList, bcolors.DARK_GREY, "", "")
+    if debug:
+        matrix.SetPoint(startPos, "S")
+        matrix.SetPoint(endPos, "E")
+
+        colorList = list()
+        colorList.append(("#", bcolors.DARK_GREY))
+        colorList.append(("o", bcolors.YELLOW))
+        colorList.append(("O", bcolors.YELLOW))
+        colorList.append(("0", bcolors.DARK_GREY))
+        colorList.append(("S", bcolors.WHITE))
+        colorList.append(("E", bcolors.WHITE))
+        matrix.PrintWithColor(colorList, bcolors.DARK_GREY, "", "")
 
     return sum
 
 def solveUnittest1(filename):
-    return solveMaze(filename, 2, 1)
+    return solveMaze(filename, 2, 1, UNITTEST.VISUAL_GRAPH_ENABLED)
 
 def solvePuzzle1(filename):
-    return solveMaze(filename, 2, 100)
+    return solveMaze(filename, 2, 100, False)
 
 def solveUnittest2(filename):
-    return solveMaze(filename, 20, 50)
+    return solveMaze(filename, 20, 50, False)
 
 def solvePuzzle2(filename):
-    return solveMaze(filename, 20, 100)
+    return solveMaze(filename, 20, 100, False)
 
 unittest(solveUnittest1, 44, "unittest1.txt")
-#unittest(solvePuzzle2, -1, "unittest1.txt")
-runCode(0,solvePuzzle1, 1307, "input.txt")
-#runCode(0,solvePuzzle2, 986545, "input.txt")
+unittest(solveUnittest2, 285, "unittest1.txt")
+runCode(20,solvePuzzle1, 1307, "input.txt")
+runCode(20,solvePuzzle2, 986545, "input.txt")
