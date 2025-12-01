7f6e8c9427efb31e8d174e74959002aedd37b18b
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2025-12-01 19:12:48 +0100

    Day 24 - Created template files

---
+++ 2024/24/Thor/solution.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+# https://adventofcode.com/2024/day/24
+
+import sys
+import re
+
+# Import custom libraries
+sys.path.insert(1, '../../../Libs')
+sys.path.insert(1, '../Libs')
+
+from advent_libs import *
+
+def solvePuzzle1(filename):
+    return 0
+
+def solvePuzzle2(filename):
+    return 0
+
+setupCode("Day 24: Crossed Wires")
+
+unittest(solvePuzzle1, -1, "unittest1.txt")
+#unittest(solvePuzzle2, -1, "unittest1.txt")
+
+#runCode(0,solvePuzzle1, -1, "input.txt")
+#runCode(0,solvePuzzle2, -1, "input.txt")
