ed0ae2c87683bd6eff47362695ceb5b0503ae980
Thor Richard Hanssen <thor.richard.hansen@gmail.com>
2025-12-06 17:08:35 +0100

    Add 2025 to run all script

---
+++ 2025/run_all.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH=/Users/thorh/Develop/DarkFactor/adventofcode/Libs
+
+for i in {1..12}
+do
+   if [ $i -lt 10 ]
+   then
+      python3 "0$i/Thor/solution.py" "PATH=0$i/Thor" "COMPACT"
+   else
+      python3 "$i/Thor/solution.py" "PATH=$i/Thor" "COMPACT"
+   fi
+done
--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=/Users/thorh/Develop/DarkFactor/adventofcode/Libs
 
-for i in {2022..2024}
+for i in {2022..2025}
 do
    cd $i
    echo "Running $i"
